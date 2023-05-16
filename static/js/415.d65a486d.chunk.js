06e11299a623e50aba6af75cf02b450bfbab3c86
EugeneAndroschuk <111381321+EugeneAndroschuk@users.noreply.github.com>
2023-05-16 22:31:22 +0000

    Deploying to gh-pages from @ EugeneAndroschuk/goit-react-hw-08-phonebook@b70bf85c8f31f852838d7b36d805c3f506e40672 🚀

---
+++ static/js/415.d65a486d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,a){a.r(t);var s=a(7253),c=a(58),l=a(3329);t.default=function(){return(0,l.jsxs)("div",{className:s.Z.container,children:[(0,l.jsx)("h1",{className:c.Z.homeTitle,children:"PHONEBOOK"}),(0,l.jsx)("div",{className:c.Z.bgdImg})]})}},58:function(e,t){t.Z={homeTitle:"Pages_homeTitle__g-Pd-",bgdImg:"Pages_bgdImg__KYlld",contactList:"Pages_contactList__--j4G",filter:"Pages_filter__aR88D"}}}]);
+//# sourceMappingURL=415.d65a486d.chunk.js.map
